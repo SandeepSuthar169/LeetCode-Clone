ab596ebd8fc38b7070ae00e4af398aa10aba8347
Sandeep Suthar <ss.suthar.004@gmail.com>
2025-11-14 20:36:42 +0530

    user schema created

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,12 +1,29 @@
 import express from "express"
 import dotenv from "dotenv"
+import cookieParser from "cookie-parser"
+import cors from "cors"
 
-
-dotenv.config()
+dotenv.config(
+    {
+        path: "./.env"
+    }
+)
 
 const app = express()
+const PORT = process.env.PORT   || 8080
 
-app.listen(process.env.PORT, ()=>{
-    console.log(`Server is runnig is ${process.env.PORT} `);
 
+app.use(cookieParser)
+app.use(cors({
+    origin: "http://localhost:5173"
+}))
+app.use(express.json())
+app.use(express.urlencoded({extended: true}))
+
+
+
+
+app.listen(PORT, () => {
+    console.log(`Backend is listening at port${PORT}`);
+    
 })
